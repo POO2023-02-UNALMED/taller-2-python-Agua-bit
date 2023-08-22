b0b6c7b130892029216fb3472e511c135474297c
= <60821161+Agua-bit@users.noreply.github.com>
2023-08-21 19:49:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ class Auto:
         return asientosTotales
 
     def verificarIntegridad(self):
-        if self.registro == Motor.registro  == Asiento.registro:
-            print("Auto original")
+        if self.registro == Motor.registro:
+            if self.registro == Asiento.registro:
+                print("Auto original")
         else:
             print("Las piezas no son originales")
 
