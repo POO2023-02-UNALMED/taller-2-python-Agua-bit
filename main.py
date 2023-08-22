c5ef43a8b7a53a8684b0420d62f0fe84e0b35f18
= <60821161+Agua-bit@users.noreply.github.com>
2023-08-21 20:14:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ class Auto:
     def verificarIntegridad(self):
         if self.registro == self.motor.registro:
             for i in range(3):
-                if self.motor.registro == self.asientos[i].registro:
-                    print("Auto original")
-                else:
-                    print("Las piezas no son originales")
+                if self.asientos[i] != None 
+                    if self.motor.registro == self.asientos[i].registro:
+                        print("Auto original")
+                    else:
+                        print("Las piezas no son originales")
 
 
 
